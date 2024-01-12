9d3909349e40218a827908525173020d842167f2
Kyle Ishie <kyle@kyleishie.com>
2024-01-12 18:12:33 -0500

    rebase upstream main fixes copyright headers

--- cmd/beskarctl/ctl/error.go
+++ cmd/beskarctl/ctl/error.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright (c) 2023, CIQ, Inc. All rights reserved
+// SPDX-FileCopyrightText: Copyright (c) 2023-2024, CIQ, Inc. All rights reserved
 // SPDX-License-Identifier: Apache-2.0
 
 package ctl
